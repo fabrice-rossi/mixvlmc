29ba675edd8ae44ca28a4c7c31afe27a890fb94c
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2022-05-27 14:03:03 +0200

    Better test name and use of identical.

--- tests/testthat/test-ctx_tree.R
+++ tests/testthat/test-ctx_tree.R
@@ -1,8 +1,8 @@
-test_that("constructor", {
+test_that("the constructor keeps the state space", {
   vals <- c("1", "A", "Q")
-  expect_equal(states(new_ctx_tree(vals)), vals)
+  expect_identical(states(new_ctx_tree(vals)), vals)
 })
 
-test_that("zero depth", {
-  expect_equal(depth(new_ctx_tree(c(0, 1))), 0)
+test_that("the empty constructor returns a zero depth tree", {
+  expect_identical(depth(new_ctx_tree(c(0, 1))), 0)
 })
