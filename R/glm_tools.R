1f8db451722f0df8737cb4a887ab39be0f8b1bcb
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2022-05-30 14:04:13 +0200

    Added core tools for glm needed by the vlmc with covariates.

---
+++ R/glm_tools.R
@@ -0,0 +1,44 @@
+prepare_covariate <- function(covariate, ctx_match, d, ...) {
+  UseMethod("prepare_covariate")
+}
+
+#' @exportS3Method
+prepare_covariate.matrix <- function(covariate, ctx_match, d, with_intercept = TRUE, ...) {
+  ncols <- ncol(covariate) * d
+  if (with_intercept) {
+    ncols <- ncols + 1
+  }
+  mm <- matrix(0, nrow = length(ctx_match), ncol = ncols)
+  if (with_intercept) {
+    mm[, 1] <- 1
+    tcol <- 2
+  } else {
+    tcol <- 1
+  }
+  for (step in 1:d) {
+    for (p in 1:ncol(covariate)) {
+      mm[, tcol] <- covariate[ctx_match + step, p]
+      tcol <- tcol + 1
+    }
+  }
+  mm
+}
+
+#' @exportS3Method
+prepare_covariate.data.frame <- function(covariate, ctx_match, d, ...) {
+  res <- list()
+  the_names <- names(covariate)
+  for (step in 1:d) {
+    for (p in 1:ncol(covariate)) {
+      res[[paste0(the_names[p], "_", step)]] <- covariate[ctx_match + step, p]
+    }
+  }
+  list2DF(res)
+}
+
+prepare_glm <- function(covariate, ctx_match, d, y) {
+  local_mm <- prepare_covariate(covariate, ctx_match, d)
+  target <- y[ctx_match + d + 1]
+  to_keep <- !is.na(target)
+  list(local_mm = local_mm[to_keep, ], target = target[to_keep])
+}
---
+++ tests/testthat/test-glm_tools.R
@@ -0,0 +1,35 @@
+test_that("the model matrix is well formed", {
+  cov_demo <- matrix(1:100, ncol = 2, byrow = FALSE)
+  ctx_pos <- seq(5, 20, by = 4)
+  ctx_length <- 5
+  mm <- prepare_covariate(cov_demo, ctx_pos, d = ctx_length)
+  expect_equal(dim(mm), c(length(ctx_pos), 1 + ncol(cov_demo) * ctx_length))
+  expect_equal(mm[, 1], rep(1, length(ctx_pos)))
+  for (ipos in seq_along(ctx_pos)) {
+    the_col <- 2
+    for (f in 1:ctx_length) {
+      for (d in 1:ncol(cov_demo)) {
+        expect_equal(mm[ipos, the_col], cov_demo[ctx_pos[ipos] + f, d])
+        the_col <- the_col + 1
+      }
+    }
+  }
+})
+
+test_that("the model data frame is well formed", {
+  cov_demo <- data.frame(x = 1:50, z = sample(c("x", "y", "z"), 50, replace = TRUE))
+  ctx_pos <- seq(5, 20, by = 4)
+  ctx_length <- 5
+  mm <- prepare_covariate(cov_demo, ctx_pos, d = ctx_length)
+  expect_equal(dim(mm), c(length(ctx_pos), ncol(cov_demo) * ctx_length))
+  expect_equal(names(mm), paste(names(cov_demo), rep(1:ctx_length, each = ncol(cov_demo)), sep = "_"))
+  for (ipos in seq_along(ctx_pos)) {
+    the_col <- 1
+    for (f in 1:ctx_length) {
+      for (d in 1:ncol(cov_demo)) {
+        expect_identical(mm[ipos, the_col], cov_demo[ctx_pos[ipos] + f, d])
+        the_col <- the_col + 1
+      }
+    }
+  }
+})
