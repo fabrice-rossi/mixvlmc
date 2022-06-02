8298498621c05d22f040c0a83b68ae7185d38901
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2022-06-02 19:47:25 +0200

    formating.

--- tests/testthat/test-glm_tools.R
+++ tests/testthat/test-glm_tools.R
@@ -2,7 +2,7 @@ test_that("the model matrix is well formed", {
   cov_demo <- matrix(1:100, ncol = 2, byrow = FALSE)
   ctx_pos <- seq(5, 20, by = 4)
   ctx_length <- 5
-  mm <- prepare_covariate(cov_demo, ctx_pos, d = ctx_length, with_intercept=TRUE)
+  mm <- prepare_covariate(cov_demo, ctx_pos, d = ctx_length, with_intercept = TRUE)
   expect_equal(dim(mm), c(length(ctx_pos), 1 + ncol(cov_demo) * ctx_length))
   expect_equal(mm[, 1], rep(1, length(ctx_pos)))
   for (ipos in seq_along(ctx_pos)) {
