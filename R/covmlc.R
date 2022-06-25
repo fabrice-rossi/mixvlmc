39178ec429f7f434263b0dab66597634e30c8802
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2022-06-25 18:51:17 +0200

    Implemented logLik for covlmc objects.

--- R/covmlc.R
+++ R/covmlc.R
@@ -102,7 +102,7 @@ ctx_tree_fit_glm <- function(tree, y, covariate, alpha, all_models = FALSE, verb
       } else if (is.null(tree[["children"]])) {
         ## let's compute the local model and return it
         ## prunable is true as there is no sub tree
-        list(model = node_fit_glm(tree, d, y, covariate, alpha, nb_vals), prunable = TRUE)
+        list(model = node_fit_glm(tree, d, y, covariate, alpha, nb_vals), prunable = TRUE, f_by = tree$f_by)
       } else {
         ## the recursive part
         ## let's get the models
@@ -154,7 +154,7 @@ ctx_tree_fit_glm <- function(tree, y, covariate, alpha, all_models = FALSE, verb
         ##      (if we have at least 2 of them)
         ##    - the current node will be prunable if this is done
 
-        result <- list()
+        result <- list(f_by = tree$f_by)
         ## do we need a local/merged model
         if (nb_rejected > 0) {
           need_local_model <- FALSE
@@ -454,4 +454,46 @@ rec_draw_covlmc <- function(prefix, rank, ival, nst, ct, vals, node2txt, merged_
 draw.covlmc <- function(ct, node2txt = draw_covlmc_node, ...) {
   rec_draw_covlmc("", 0, 1, length(ct$vals), ct, ct$vals, node2txt, draw_covlmc_merged, ...)
   invisible(ct)
+}
+
+rec_loglikelihood_covlmc <- function(tree) {
+  if (is.null(tree)) {
+    list(ll = 0, df = 0L, nobs = 0L)
+  } else if (is.null(tree$children)) {
+    if (is.null(tree$model)) {
+      list(ll = 0, df = 0L, nobs = 0L)
+    } else {
+      list(
+        ll = tree$model$likelihood, df = length(tree$model$coefficients),
+        nobs = sum(tree$f_by)
+      )
+    }
+  } else {
+    ## take care of the local model
+    sub_ll <- list(ll = 0, df = 0L, nobs = 0L)
+    for (v in seq_along(tree$children)) {
+      ch_ll <- rec_loglikelihood_covlmc(tree$children[[v]])
+      sub_ll$ll <- sub_ll$ll + ch_ll$ll
+      sub_ll$df <- sub_ll$df + ch_ll$df
+      sub_ll$nobs <- sub_ll$nobs + ch_ll$nobs
+    }
+    if (is.null(tree[["merged_model"]])) {
+      sub_ll
+    } else {
+      sub_ll$ll <- sub_ll$ll + tree$merged_model$likelihood
+      sub_ll$df <- sub_ll$df + length(tree$megred_model$coefficients)
+      sub_ll$nobs <- sub_ll$nobs + length(tree$merged_model$data$target)
+      sub_ll
+    }
+  }
+}
+
+#' @export
+logLik.covlmc <- function(object, ...) {
+  pre_res <- rec_loglikelihood_covlmc(object)
+  ll <- pre_res$ll
+  attr(ll, "df") <- pre_res$df
+  attr(ll, "nobs") <- pre_res$nobs
+  class(ll) <- "logLik"
+  ll
 }
