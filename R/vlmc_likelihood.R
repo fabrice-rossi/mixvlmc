6b1a29f05f4a46a4472de328cd3f6398a73a4fa7
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2022-06-26 17:17:32 +0200

    Complete reimplementation of likelihood calculation that fixed a major.
    In the previous version, the likelihood of a new time series for a fitted model was considering only the context tree to be fixed, disregarding the associated probabilities. This is fixed in this version.

--- R/vlmc_likelihood.R
+++ R/vlmc_likelihood.R
@@ -1,59 +1,40 @@
-dispatch_in_ctx_tree <- function(tree, x) {
-  recurse_dispatch <- function(tree, x, nb_vals, d, from, f_by) {
-    if (is.null(tree$children)) {
-      list(f_by = f_by)
-    } else {
-      fmatch <- forward_match_all_ctx_counts(x, nb_vals, d, from)
-      children <- vector(mode = "list", nb_vals)
-      nb_children <- 0
-      for (v in 1:nb_vals) {
-        if (length(fmatch$positions[[v]]) > 0 && length(tree$children[[v]]) > 0) {
-          children[[v]] <- recurse_dispatch(tree$children[[v]], x, nb_vals, d + 1, fmatch$positions[[v]], fmatch$counts[v, ])
-          nb_children <- nb_children + 1
-        } else {
-          children[[v]] <- list()
-        }
-      }
-      if (nb_children > 0) {
-        result <- list(children = children)
-      } else {
-        result <- list()
-      }
-      result$f_by <- f_by
-      result
-    }
+local_loglikelihood_vlmc <- function(model_counts, data_counts = NULL) {
+  if (is.null(data_counts)) {
+    data_counts <- model_counts
   }
-  recurse_dispatch(tree, x, length(tree$vals), 0, NULL, table(x))
-}
-
-local_loglikelihood <- function(counts) {
-  sc <- sum(counts)
+  sc <- sum(model_counts)
   if (sc > 0) {
-    probs <- counts / sum(counts)
-    sum(counts * ifelse(probs > 0, log(probs), 0))
+    probs <- model_counts / sc
+    sum(data_counts * ifelse(probs > 0, log(probs), 0))
   } else {
     0
   }
 }
 
-rec_loglikelihood <- function(tree) {
+rec_loglikelihood_vlmc <- function(tree) {
   if (is.null(tree$f_by)) {
     # place holder list
-    0
+    NA
   } else if (is.null(tree$children)) {
     ## simple leaf case
-    local_loglikelihood(tree$f_by)
+    local_loglikelihood_vlmc(tree$f_by, tree$data_f_by)
   } else {
     ## recursive case
-    sub_ll <- sum(sapply(tree$children, rec_loglikelihood))
+    all_ll <- sapply(tree$children, rec_loglikelihood_vlmc)
+    sub_ll <- sum(all_ll, na.rm = TRUE)
     ## is the node a valid context
-    nst <- nb_sub_tree(tree)
-    if (nst < length(tree$f_by)) {
+    if (anyNA(all_ll)) {
       ## let us add the local contribution
       sub_trees <- sapply(tree$children, function(x) !is.null(x$f_by))
       sub_counts <- rowSums(sapply(tree$children[sub_trees], function(x) x$f_by))
       loc_counts <- tree$f_by - sub_counts
-      sub_ll <- sub_ll + local_loglikelihood(loc_counts)
+      if (is.null(tree$data_f_by)) {
+        sub_ll <- sub_ll + local_loglikelihood_vlmc(loc_counts)
+      } else {
+        data_sub_counts <- rowSums(sapply(tree$children[sub_trees], function(x) x$data_f_by))
+        data_loc_counts <- tree$data_f_by - data_sub_counts
+        sub_ll <- sub_ll + local_loglikelihood_vlmc(loc_counts, data_loc_counts)
+      }
     }
     sub_ll
   }
@@ -61,7 +42,7 @@ rec_loglikelihood <- function(tree) {
 
 #' @export
 logLik.vlmc <- function(object, ...) {
-  ll <- rec_loglikelihood(object)
+  ll <- rec_loglikelihood_vlmc(object)
   attr(ll, "df") <- object$nb_ctx * (length(object$vals) - 1)
   attr(ll, "nobs") <- sum(object$f_by)
   class(ll) <- "logLik"
@@ -83,10 +64,10 @@ logLik.vlmc <- function(object, ...) {
 loglikelihood <- function(vlmc, x = NULL) {
   assertthat::assert_that(is_vlmc(vlmc))
   if (is.null(x)) {
-    rec_loglikelihood(vlmc)
+    rec_loglikelihood_vlmc(vlmc)
   } else {
     nx <- to_dts(x, vlmc$vals)
-    nvlmc <- dispatch_in_ctx_tree(vlmc, nx$ix)
-    rec_loglikelihood(nvlmc)
+    nvlmc <- match_ctx(vlmc, nx$ix)
+    rec_loglikelihood_vlmc(nvlmc)
   }
 }
