c9754d1841455ab7fd76132e37a554c2cf43996a
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2022-06-26 17:17:45 +0200

    More simple tests.

--- devutils/simulated-data-tests.R
+++ devutils/simulated-data-tests.R
@@ -15,7 +15,7 @@ x <- rmarkovchain(1000, basic)
 x_mc <- markovchainFit(x)
 x_homc <- fitHigherOrder(x, 2)
 
-full_vlmc <- vlmc(x, alpha = 0.2)
+full_vlmc <- vlmc(x, alpha = 0.15)
 alphas <- cutoff(full_vlmc, mode="quantile")
 
 x_vlmcs <- vector(mode = "list", length(alphas))
@@ -26,4 +26,21 @@ for (k in seq_along(alphas)) {
 }
 
 x_vlmcs_bic <- sapply(x_vlmcs, BIC)
+x_vlmcs_aic <- sapply(x_vlmcs, AIC)
 x_vlmcs_nb_ctx <- sapply(x_vlmcs, context_number)
+x_vlmc <- x_vlmcs[[which.min(x_vlmcs_bic)]]
+
+TM2 <- matrix(c(0.6, 0.4, 0.25, 0.75),
+             ncol = 2,
+             byrow = TRUE
+)
+basic2 <- new("markovchain",
+             states = c("on", "off"), transitionMatrix = TM2,
+             name = "basic2"
+)
+x2 <- rmarkovchain(1000, basic2)
+x2_vlmc <- vlmc(x2, alpha=alphas[which.min(x_vlmcs_bic)])
+print(loglikelihood(x_vlmc, x))
+print(loglikelihood(x_vlmc, x2))
+print(loglikelihood(x2_vlmc, x))
+print(loglikelihood(x2_vlmc, x2))
