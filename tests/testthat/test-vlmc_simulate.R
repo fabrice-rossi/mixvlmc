71792f8a819ad2924ff4ca3c77fb0b06e870ea33
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2022-07-12 23:27:38 +0200

    New tests for basic consistency of simulate.vlmc.

---
+++ tests/testthat/test-vlmc_simulate.R
@@ -0,0 +1,19 @@
+test_that("vlmc simulation generates a consistent sample", {
+  for(k in 2:4) {
+    data_set <- build_markov_chain(1000, k, seed = k)
+    x_vlmc <- vlmc(data_set$x)
+    xs <- simulate(x_vlmc, 50, seed = 2 * k + 1)
+    expect_equal(length(xs), 50)
+    expect_identical(levels(xs), states(x_vlmc))
+  }
+})
+
+test_that("vlmc simulation generates always the same sample with the same seed", {
+  for(k in 2:4) {
+    data_set <- build_markov_chain(1000, k, seed = k)
+    x_vlmc <- vlmc(data_set$x)
+    xs <- simulate(x_vlmc, 50, seed = 2 * k + 1)
+    xs2 <- simulate(x_vlmc, 50, seed = 2 * k + 1)
+    expect_identical(xs2, xs)
+  }
+})
