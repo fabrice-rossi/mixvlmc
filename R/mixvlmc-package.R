9b5c9855f181561323067bd8de2ecfaa6032320d
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2022-07-28 12:37:59 +0200

    Fix usethis usage.

--- R/mixvlmc-package.R
+++ R/mixvlmc-package.R
@@ -1,7 +1,10 @@
 #'
+## usethis namespace: start
 #' @import Rcpp
 #' @importFrom Rcpp evalCpp
 #' @importFrom stats simulate
+#' @importFrom stats predict
 #' @useDynLib mixvlmc
+## usethis namespace: end
 NULL
 #>
