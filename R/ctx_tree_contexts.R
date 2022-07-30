3e0bd4c0f84e60ef53b43a1949f5757c03268038
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2022-07-30 19:41:45 +0200

    Improve contexts.ctx_tree (issue #7).
    The new version of contexts.ctx_tree is more generic and is the first step in implementing feature #7.

--- R/ctx_tree_contexts.R
+++ R/ctx_tree_contexts.R
@@ -25,20 +25,66 @@ rec_contexts <- function(path, ct, vals) {
   }
 }
 
+rec_contexts_extractor <- function(path, ct, vals, extractor, control) {
+  if (is.null(ct$children)) {
+    ## this is a leaf
+    ## if there is a f_by, then this is a context
+    if (is.null(ct[["f_by"]])) {
+      NULL
+    } else {
+      content <- extractor(ct, control)
+      cbind(data.frame(context = I(list(path))), content)
+    }
+  } else {
+    all_ctx <- NULL
+    for (v in seq_along(ct$children)) {
+      if (is.null(path)) {
+        sub_path <- vals[v]
+      } else {
+        sub_path <- c(vals[v], path)
+      }
+      sub_ctx <- rec_contexts_extractor(sub_path, ct$children[[v]], vals, extractor, control)
+      all_ctx <- rbind(all_ctx, sub_ctx)
+    }
+    if (nb_sub_tree(ct) < length(vals)) {
+      all_ctx <- rbind(
+        all_ctx,
+        data.frame(
+          context = I(list(path)),
+          extractor(ct, control)
+        )
+      )
+    }
+    all_ctx
+  }
+}
+
+ctx_context_extractor <- function(ctx, control) {
+  res <- data.frame(freq = sum(ctx[["f_by"]]))
+  if (!is.null(control[["vals"]])) {
+    freq_by_val <- as.list(ctx[["f_by"]])
+    names(freq_by_val) <- as.character(control[["vals"]])
+    res <- cbind(res, data.frame(freq_by_val))
+  }
+  res
+}
+
 #' Contexts of a context tree
 #'
-#' This function extracts from a context tree the list of all its contexts.
+#' This function extracts from a context tree a description of all of its
+#' contexts.
 #'
-#' The default behavior consists in returnin a list of all the contexts
+#' The default behavior consists in returning a list of all the contexts
 #' contained in the tree (with \code{type="list"}). When
-#' \code{type="data.frame"}, the method returns a data.frame whose first column
-#' contains the contexts. Other columns contain context specific values which
-#' depend on the actual class of the tree.
+#' \code{type="data.frame"}, the method returns a data.frame whose first column,
+#' named \code{context}, contains the contexts. Other columns contain context
+#' specific values which depend on the actual class of the tree and on
+#' additional parameters.
 #'
-#' Notice that contexts are given by default in their left to right reading
-#' order. For instance, the context c(0, 1) is reported if the sequence 0, then
-#' 1, is registered in the context tree. Set reverse to \code{TRUE} for the
-#' reverse convention.
+#' @section State order in a context: Notice that contexts are given by default
+#'   in their left to right reading order. For instance, the context c(0, 1) is
+#'   reported if the sequence 0, then 1, is registered in the context tree. Set
+#'   reverse to \code{TRUE} for the reverse convention.
 #'
 #' @param ct a context tree.
 #' @param type result type (see details).
@@ -57,21 +103,59 @@ contexts <- function(ct, type = c("list", "data.frame"), reverse = FALSE, ...) {
   UseMethod("contexts")
 }
 
+#' @inherit contexts
+#' @param frequency specify the counts to be included in the result data.frame.
+#'   The default value of \code{NULL} does not include anything. \code{"total"}
+#'   gives the number of occurrences of each context in the original sequence.
+#'   \code{"detailed"} includes in addition the break down of these occurrences
+#'   into all the possible states.
+#' @details The default result for \code{type="list"} and \code{frequency=NULL}
+#'   is the list of all contexts.
+#'
+#'   Other results are obtained only with \code{type="data.frame"}. In this case
+#'   the resulting \code{data.frame} has a \code{context} column storing the
+#'   contexts. If \code{frequency="total"}, an additional column named
+#'   \code{freq} gives the number of occurrences of each context in the series
+#'   used to build the tree. If \code{frequency="detailed"}, one additional
+#'   column is added per state in the context space. Each column records the
+#'   number of times a given context is followed by the corresponding value in
+#'   the original series.
+#' @examples
+#' dts <- sample(as.factor(c("A", "B", "C")), 100, replace = TRUE)
+#' dts_tree <- ctx_tree(dts, max_depth = 3, min_size = 5)
+#' contexts(dts_tree)
+#' contexts(dts_tree, "data.frame", frequency = "total")
+#' contexts(dts_tree, "data.frame", frequency = "detailed")
 #' @export
-contexts.ctx_tree <- function(ct, type = c("list", "data.frame"), reverse = FALSE, ...) {
+contexts.ctx_tree <- function(ct, type = c("list", "data.frame"), reverse = FALSE, frequency = NULL, ...) {
   type <- match.arg(type)
-  preres <- rec_contexts(NULL, ct, ct$vals)
-  if (reverse) {
-    preres <- lapply(preres, rev)
-  }
-  if (is.null(preres[[length(preres)]])) {
-    ## root context
-    preres[[length(preres)]] <- ct$vals[0]
-  }
-  if (type == "list") {
-    preres
+  if (missing(frequency)) {
+    preres <- rec_contexts(NULL, ct, ct$vals)
+    if (reverse) {
+      preres <- lapply(preres, rev)
+    }
+    if (is.null(preres[[length(preres)]])) {
+      ## root context
+      preres[[length(preres)]] <- ct$vals[0]
+    }
+    if (type == "list") {
+      preres
+    } else {
+      data.frame(context = I(preres))
+    }
   } else {
-    data.frame(contexts = I(preres))
+    assertthat::assert_that(type == "data.frame")
+    assertthat::assert_that(frequency %in% c("total", "detailed"))
+    control <- NULL
+    if (frequency == "detailed") {
+      control <- list(vals = ct$vals)
+    }
+    preres <- rec_contexts_extractor(NULL, ct, ct$vals, ctx_context_extractor, control)
+    if (reverse) {
+      new_res <- data.frame(context = I(lapply(preres$context, rev)))
+      preres <- cbind(new_res, preres[2:ncol(preres)])
+    }
+    preres
   }
 }
 
--- tests/testthat/test-ctx_tree_contexts.R
+++ tests/testthat/test-ctx_tree_contexts.R
@@ -24,3 +24,41 @@ test_that("the context tree construction finds the specified contexts", {
   contexts <- list(c(0, 0), c(0, 1), c(1, 0), c(1, 1))
   expect_identical(contexts(dts_ctree, reverse = TRUE), contexts)
 })
+
+test_that("contexts return the appropriate data format", {
+  dts <- sample(c("A", "B", "C"), 100, replace = TRUE)
+  dts_tree <- ctx_tree(dts, max_depth = 4)
+  expect_type(contexts(dts_tree), "list")
+  expect_s3_class(contexts(dts_tree, type = "data.frame"), "data.frame")
+})
+
+test_that("contexts reversing reverses", {
+  dts <- sample(c("A", "B", "C"), 100, replace = TRUE)
+  dts_tree <- ctx_tree(dts, max_depth = 4)
+  def_ctx <- contexts(dts_tree)
+  rev_ctx <- contexts(dts_tree, reverse = TRUE)
+  expect_length(rev_ctx, length(def_ctx))
+  for (k in seq_along(def_ctx)) {
+    expect_identical(rev_ctx[[k]], rev(def_ctx[[k]]))
+  }
+})
+
+test_that("format consistency", {
+  dts <- sample(c("A", "B", "C"), 100, replace = TRUE)
+  dts_tree <- ctx_tree(dts, max_depth = 4)
+  raw_ctx <- contexts(dts_tree, type = "data.frame")
+  expect_named(raw_ctx, c("context"))
+  freq_ctx <- contexts(dts_tree, type = "data.frame", frequency = "total")
+  expect_named(freq_ctx, c("context", "freq"))
+  full_ctx <- contexts(dts_tree, type = "data.frame", frequency = "detailed")
+  expect_named(full_ctx, c("context", "freq", "A", "B", "C"))
+})
+
+test_that("basic count consistency", {
+  dts <- sample(c("A", "B", "C"), 100, replace = TRUE)
+  dts_tree <- ctx_tree(dts, max_depth = 4)
+  full_ctx <- contexts(dts_tree, type = "data.frame", frequency = "detailed")
+  expect_equal(nrow(full_ctx), context_number(dts_tree))
+  totals <- apply(full_ctx[3:5], 1, sum)
+  expect_equal(full_ctx$freq, totals)
+})
