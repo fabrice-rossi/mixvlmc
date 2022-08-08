99d8f9bbda2d5a74481f558e52c2c936c16ebb02
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2022-08-08 19:20:23 +0200

    Generate integer valued time series

--- tests/testthat/helper-vlmc.R
+++ tests/testthat/helper-vlmc.R
@@ -1,14 +1,15 @@
 build_markov_chain <- function(n, nb_vals, seed = 0) {
+  nb_vals <- as.integer(nb_vals)
   withr::local_seed(seed)
   TM <- matrix(NA, ncol = nb_vals, nrow = nb_vals)
   for (k in 1:nb_vals) {
     TM[k, ] <- runif(nb_vals)
     TM[k, ] <- TM[k, ] / sum(TM[k, ])
   }
-  x <- rep(0, n)
-  x[1] <- sample(0:(nb_vals - 1), 1)
+  x <- rep(0L, n)
+  x[1] <- sample(0L:(nb_vals - 1L), 1)
   for (k in 2:n) {
-    x[k] <- sample(0:(nb_vals - 1), 1, prob = TM[x[k - 1] + 1, ])
+    x[k] <- sample(0L:(nb_vals - 1L), 1, prob = TM[x[k - 1] + 1, ])
   }
   list(x = x, TM = TM)
 }
