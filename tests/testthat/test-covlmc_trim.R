52e6e24ace3cb1ac3d975989d3a7f086e7b7104f
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2023-07-13 19:54:08 +0200

    Add more tests for trimmed models.

--- tests/testthat/test-covlmc_trim.R
+++ tests/testthat/test-covlmc_trim.R
@@ -19,6 +19,45 @@ test_that("trimming removes only what it should remove", {
       contexts(m_cov, hsize = TRUE, model = "coef", frequency = "detailed", metrics = TRUE, merging = TRUE),
       contexts(t_m_cov, hsize = TRUE, model = "coef", frequency = "detailed", metrics = TRUE, merging = TRUE)
     )
+    ## still usable with keep_mode==TRUE
+    expect_equal(
+      loglikelihood(m_cov, dts, dts_cov),
+      loglikelihood(t_m_cov_model, dts, dts_cov)
+    )
+    expect_no_error(contexts(t_m_cov_model, hsize = TRUE, model = "full"))
+    expect_equal(
+      simulate(m_cov, nsim = 50, seed = 0, dts_cov),
+      simulate(t_m_cov_model, nsim = 50, seed = 0, dts_cov)
+    )
+    ## errors
+    expect_error(
+      contexts(t_m_cov, hsize = TRUE, model = "full"),
+      "Full model extraction is not supported by fully trimmed covlmc"
+    )
+    expect_error(
+      loglikelihood(t_m_cov, dts, dts_cov),
+      "loglikelihood calculation for new data is not supported by fully trimmed covlmc"
+    )
+    expect_error(
+      simulate(t_m_cov, nsim = 50, seed = 0, dts_cov),
+      "simulate is not supported by fully trimmed covlmc"
+    )
+    expect_error(
+      metrics(t_m_cov),
+      "metrics is not supported by trimmed covlmc"
+    )
+    expect_error(
+      metrics(t_m_cov_model),
+      "metrics is not supported by trimmed covlmc"
+    )
+    expect_error(
+      prune(t_m_cov),
+      "covlmc must be called with keep_data=TRUE to enable post pruning"
+    )
+    expect_error(
+      prune(t_m_cov_model),
+      "covlmc must be called with keep_data=TRUE to enable post pruning"
+    )
   }
 })
 
