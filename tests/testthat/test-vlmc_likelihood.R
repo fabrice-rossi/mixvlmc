cb7c4b7cf5a9bee5201b0a2e34b28abfefda0a24
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2023-08-11 17:23:29 +0200

    Add more tests for vlmc likelihood calculation.

--- tests/testthat/test-vlmc_likelihood.R
+++ tests/testthat/test-vlmc_likelihood.R
@@ -3,3 +3,31 @@ test_that("the likelihood calculation is valid", {
   x_tree <- vlmc(x, alpha = 0.05)
   expect_identical(loglikelihood(x_tree, x), loglikelihood(x_tree))
 })
+
+test_that("the logLik output format is valid", {
+  x <- sample(c(TRUE, FALSE), 1000, replace = TRUE)
+  x_tree <- vlmc(x, alpha = 0.05)
+  x_tree_ll <- logLik(x_tree)
+  expect_s3_class(x_tree_ll, "logLik")
+  expect_named(attributes(x_tree_ll), c("df", "nobs", "class"))
+})
+
+test_that("the logLik results are correct", {
+  withr::local_seed(0)
+  for (k in 1:9) {
+    x <- sample(0:k, 1000 + 100 * k, replace = TRUE)
+    x_tree <- vlmc(x, alpha = 0.05)
+    x_tree_ll <- logLik(x_tree)
+    expect_equal(attr(x_tree_ll, "nobs"), length(x))
+    expect_equal(attr(x_tree_ll, "df"), k * context_number(x_tree))
+  }
+})
+
+test_that("logLik and loglikelihood report the same value when they should", {
+  withr::local_seed(0)
+  for (k in 1:9) {
+    x <- sample(0:k, 1000 + 100 * k, replace = TRUE)
+    x_tree <- vlmc(x, alpha = 0.05)
+    expect_equal(as.numeric(logLik(x_tree)), as.numeric(loglikelihood(x_tree)))
+  }
+})
