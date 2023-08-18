7f9b66f30ff15fb2f53418a97b289e06b20c3593
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2023-08-18 16:18:54 +0200

    Do not expand a node when no match can be found.

--- R/match_ctx.R
+++ R/match_ctx.R
@@ -1,6 +1,9 @@
 #' Match a context tree to a new discrete time series
 #'
-#' This is an internal function.
+#' This is an internal function. It adds to a context tree a new member to each
+#' node, data_f_by. The vector contains the counts of occurrences of symbols
+#' after each of the context of the tree in the time series x. In addition
+#' it truncates the sub tree whose contexts are not matched in the time series.
 #'
 #' @param tree a context tree.
 #' @param x a discrete time series in its low level representation.
@@ -24,8 +27,12 @@ match_ctx <- function(tree, x, keep_match = FALSE) {
       nb_children <- 0
       for (v in 1:nb_vals) {
         if (length(fmatch$positions[[v]]) > 0 && length(tree$children[[v]]) > 0) {
-          children[[v]] <- recurse_match_ctx(tree$children[[v]], x, nb_vals, d + 1, fmatch$positions[[v]], fmatch$counts[v, ])
-          nb_children <- nb_children + 1
+          if (any(fmatch$counts[v, ] > 0)) {
+            children[[v]] <- recurse_match_ctx(tree$children[[v]], x, nb_vals, d + 1, fmatch$positions[[v]], fmatch$counts[v, ])
+            nb_children <- nb_children + 1
+          } else {
+            children[[v]] <- list()
+          }
         } else {
           children[[v]] <- list()
         }
