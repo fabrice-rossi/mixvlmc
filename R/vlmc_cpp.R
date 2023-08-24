fda3f051058321420b4800d8a5841a1210211236
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2023-08-24 12:09:36 +0200

    Implement prune.vlmc for the C++ backend (issue #52).

--- R/vlmc_cpp.R
+++ R/vlmc_cpp.R
@@ -13,3 +13,37 @@ print.vlmc_cpp <- function(x, ...) {
   }
   invisible(x)
 }
+
+#' @rdname prune
+#' @export
+prune.vlmc_cpp <- function(vlmc, alpha = 0.05, cutoff = NULL, ...) {
+  if (extptr_is_null(vlmc$root$.pointer)) {
+    stop("Missing C++ representation!\nThis object was probably restored from a saved object.\n")
+  }
+  if (is.null(cutoff)) {
+    if (is.null(alpha) || !is.numeric(alpha) || alpha <= 0 || alpha > 1) {
+      stop("the alpha parameter must be in (0, 1]")
+    }
+  }
+  if (is.null(cutoff)) {
+    cutoff <- to_native(alpha, length(vlmc$vals))
+  } else {
+    ## cutoff takes precedence
+    alpha <- to_quantile(cutoff, length(vlmc$vals))
+  }
+  pre_result <- vlmc$root$clone_prune_context(1L, -1L, cutoff)
+  result <- new_ctx_tree_cpp(vlmc$vals, pre_result, class = c("vlmc_cpp", "vlmc"))
+  result$data_size <- vlmc$data_size
+  result$alpha <- alpha
+  result$cutoff <- cutoff
+  ## recompute the extended_ll
+  if (depth(result) > 0) {
+    result$ix <- vlmc$ix[1:min(depth(result), length(vlmc$ix))]
+    result$extended_ll <- result$root$loglikelihood(result$ix, TRUE, FALSE)
+  } else {
+    result$extended_ll <- 0
+  }
+  ## preserve the construction information
+  result$max_depth <- vlmc$max_depth
+  result
+}
--- tests/testthat/test-vlmc_cpp.R
+++ tests/testthat/test-vlmc_cpp.R
@@ -19,3 +19,44 @@ test_that("the C++ context algorithm selects the same PST as the R implementatio
   }
   expect_true(all_valid)
 })
+
+test_that("post pruning is equivalent to direct pruning (alpha, C++)", {
+  pc <- powerconsumption[powerconsumption$week == 5, ]
+  dts <- cut(pc$active_power, breaks = c(0, quantile(pc$active_power, probs = c(0.25, 0.5, 0.75, 1))))
+  model <- vlmc(dts, alpha = 0.1, backend = "C++")
+  r_model <- vlmc(dts, alpha = 0.1)
+  cut_off <- cutoff(r_model)
+  for (k in seq_along(cut_off)) {
+    pruned_model <- prune(model, alpha = cut_off[k])
+    direct_model <- vlmc(dts, alpha = cut_off[k], backend = "C++")
+    expect_true(compare_vlmc(pruned_model, direct_model))
+  }
+})
+
+test_that("post pruning is equivalent to direct pruning (cutoff, C++)", {
+  pc <- powerconsumption[powerconsumption$week == 5, ]
+  dts <- cut(pc$active_power, breaks = c(0, quantile(pc$active_power, probs = c(0.25, 0.5, 0.75, 1))))
+  model <- vlmc(dts, alpha = 0.1, backend = "C++")
+  r_model <- vlmc(dts, alpha = 0.1)
+  cut_off <- cutoff(r_model, mode = "native")
+  for (k in seq_along(cut_off)) {
+    pruned_model <- prune(model, cutoff = cut_off[k])
+    direct_model <- vlmc(dts, cutoff = cut_off[k], backend = "C++")
+    expect_true(compare_vlmc(pruned_model, direct_model))
+  }
+})
+
+test_that("alpha pruning is equivalent to cutoff pruning (C++)", {
+  pc <- powerconsumption[powerconsumption$week == 5, ]
+  dts <- cut(pc$active_power, breaks = c(0, quantile(pc$active_power, probs = c(0.25, 0.5, 0.75, 1))))
+  model <- vlmc(dts, alpha = 0.1, backend = "C++")
+  r_model <- vlmc(dts, alpha = 0.1)
+  c_cut_off <- cutoff(r_model, mode = "native")
+  a_cut_off <- cutoff(r_model, mode = "quantile")
+  expect_length(a_cut_off, length(c_cut_off))
+  for (k in seq_along(a_cut_off)) {
+    a_pruned_model <- prune(model, alpha = a_cut_off[k])
+    c_pruned_model <- prune(model, cutoff = c_cut_off[k])
+    expect_true(compare_vlmc(a_pruned_model, c_pruned_model))
+  }
+})
