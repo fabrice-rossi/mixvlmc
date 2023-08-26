667f50b2ecf633d81e6de9f20176b7597e89b29b
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2023-08-26 17:55:04 +0200

    Reduce the number of significant digits used to print the quantile cut off.

--- R/vlmc.R
+++ R/vlmc.R
@@ -342,7 +342,7 @@ print.vlmc <- function(x, ...) {
     "VLMC context tree on",
     paste(x$vals, collapse = ", ")
   ), "\n")
-  cat(paste(" cutoff: ", signif(x$cutoff, 4), " (quantile: ", x$alpha, ")\n", sep = ""))
+  cat(paste(" cutoff: ", signif(x$cutoff, 4), " (quantile: ", signif(x$alpha, 4), ")\n", sep = ""))
   if (!is.null(x$nb_ctx)) {
     cat(paste(" Number of contexts:", x$nb_ctx, "\n"))
   }
