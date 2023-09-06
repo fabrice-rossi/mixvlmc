fa1757925e4bc3b9e6c1cdc86be2d503ba923644
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2023-09-06 10:04:17 +0200

    Simplified the interface of predict.vlmc and add more documention.
    To mimic predict.* functions, we return either probabilities or actual predictions,
    not both at the same time.
    
    We use caret conventions of type="raw" and type="probs".

--- R/vlmc_predict.R
+++ R/vlmc_predict.R
@@ -1,76 +1,85 @@
-#' Predictor of the following state of a discrete time series for a vlmc
+#' Next state prediction in a discrete time series for a VLMC
 #'
-#' This function predict each state of a time series from the distribution estimated by the
-#' given vlmc object.
+#' This function computes one step ahead predictions for a discrete time series
+#' based on a VLMC.
 #'
+#' Given a time series `X`, at time step `t`, a context is computed using
+#' observations from `X[1]` to `X[t-1]`. The prediction is then the most
+#' probable state for `X[t]` given this contexts. Ties are broken according to
+#' the natural order in the state space, favouring "small" values. The time
+#' series of predictions is returned by the function when `type="raw"` (default
+#' case).
+#'
+#' When `type="probs"`, each `X[t]` is associated to the conditional probabilities
+#' of the next state given the context. Those probabilities are returned as a matrix
+#' of probabilities with column names given by the state names.
 #'
 #' @param object a fitted vlmc object.
-#' @param ts a time series adapted to the vlmc object.
-#' @param type charachter indicating the type of prediction required.
-#' The default "class" returns the predictor,
-#' "probs" returns a matrix of prediction probabilities.
+#' @param newdata a time series adapted to the vlmc object.
+#' @param type character indicating the type of prediction required. The default
+#'  `"raw"` returns actual predictions in the form of a new time series. The
+#'  alternative `"probs"` returns a matrix of prediction probabilities (see details).
 #' @param ... additional arguments.
 #'
+#' @returns A vector of predictions if `type="raw"` or a matrix of state probabilities
+#'   if `type="probs"`.
 #' @export
 #' @examples
 #' pc <- powerconsumption[powerconsumption$week == 5, ]
 #' dts <- cut(pc$active_power, breaks = c(0, quantile(pc$active_power, probs = c(0.25, 0.5, 0.75, 1))))
 #' model <- vlmc(dts, min_size = 5)
 #' predict(model, dts[1:5])
-predict.vlmc <- function(object, ts, type = "class", ...) {
+#' predict(model, dts[1:5], "probs")
+predict.vlmc <- function(object, newdata, type = c("raw", "probs"), ...) {
+  type <- match.arg(type)
   max_depth <- depth(object)
-  if (!is.null(ts)) {
-    assertthat::assert_that((typeof(ts) == typeof(object$vals)) && (class(ts) == class(object$vals)),
-                            msg = "ts is not compatible with the model state space"
+  if (!is.null(newdata)) {
+    assertthat::assert_that((typeof(newdata) == typeof(object$vals)) && (class(newdata) == class(object$vals)),
+      msg = "newdata is not compatible with the model state space"
     )
-    dts <- to_dts(ts, object$vals)
+    dts <- to_dts(newdata, object$vals)
     ctx <- rev(dts$ix) + 1
   } else {
-    stop("ts is empty.")
+    stop("newdata is empty.")
   }
   pred_vals <- object$vals ### ATTENTION
   MAT <- NULL
-  if ("class" %in% type) {
+  if (type == "raw") {
     if (max_depth == 0) {
-      pred <- which.max(object$f_by)
-      thing <- matrix(rep(pred, (length(ctx) + 1)), ncol = 1)
+      pred <- which.max(object$f_by) ## implements the tie break rule by default
+      thing <- rep(pred, (length(newdata) + 1))
     } else {
-      thing <- matrix(nrow = (length(ctx) + 1), ncol = 1)
-      thing[1, ] <- which.max(object$f_by)
-      for (ii in 1:length(ctx)) {
+      thing <- rep(0L, (length(newdata) + 1))
+      thing[1] <- which.max(object$f_by)
+      for (ii in 1:length(newdata)) {
         if (ii <= max_depth) {
           ac_ctx <- ctx[(length(ctx) - ii + 1):length(ctx)]
-          thing[1 + ii, ] <- which.max(match_context(object, ac_ctx)$tree$f_by)
         } else {
           ac_ctx <- ctx[(length(ctx) - ii + 1):(length(ctx) - ii + max_depth)]
-          thing[1 + ii, ] <- which.max(match_context(object, ac_ctx)$tree$f_by)
         }
+        thing[1 + ii] <- which.max(match_context(object, ac_ctx)$tree$f_by)
       }
     }
-    MAT <- cbind(MAT, matrix(pred_vals[thing[, 1]], ncol = 1))
-    colnames(MAT) <- "class"
-  }
-  if ("probs" %in% type) {
+    MAT <- pred_vals[thing]
+  } else {
     if (max_depth == 0) {
       prob <- object$f_by / sum(object$f_by)
-      probM <- matrix(rep(prob, (length(ctx) + 1)), ncol = length(pred_vals), byrow = T)
+      probM <- matrix(rep(prob, each = (length(newdata) + 1)), ncol = length(pred_vals))
     } else {
-      probM <- matrix(nrow = (length(ctx) + 1), ncol = length(pred_vals))
+      probM <- matrix(NA, nrow = (length(newdata) + 1), ncol = length(pred_vals))
       probM[1, ] <- object$f_by / sum(object$f_by)
-      for (ii in 1:length(ctx)) {
+      for (ii in 1:length(newdata)) {
         if (ii <= max_depth) {
           ac_ctx <- ctx[(length(ctx) - ii + 1):length(ctx)]
-          ac_prob <- match_context(object, ac_ctx)$tree$f_by
-          probM[1 + ii, ] <- ac_prob / sum(ac_prob)
         } else {
           ac_ctx <- ctx[(length(ctx) - ii + 1):(length(ctx) - ii + max_depth)]
-          ac_prob <- match_context(object, ac_ctx)$tree$f_by
-          probM[1 + ii, ] <- ac_prob / sum(ac_prob)
         }
+        ac_prob <- match_context(object, ac_ctx)$tree$f_by
+        probM[1 + ii, ] <- ac_prob / sum(ac_prob)
       }
     }
-    colnames(probM) <- paste(pred_vals)
-    MAT <- cbind(MAT, probM)
+    colnames(probM) <- as.character(pred_vals)
+    MAT <- probM
   }
   return(MAT)
 }
