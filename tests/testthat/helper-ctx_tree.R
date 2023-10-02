698d13017177ecd5e6a77beaf84d0b2ae8250e3a
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2023-10-02 15:01:02 +0200

    Add slow versions of counts and occurrences search for test purposes.

--- tests/testthat/helper-ctx_tree.R
+++ tests/testthat/helper-ctx_tree.R
@@ -21,3 +21,45 @@ build_demo_tree <- function(vals, depth) {
   pre_res$vals <- vals
   new_ctx_tree(vals, pre_res, compute_stats = FALSE)
 }
+
+## works only for integers (1:v)
+count_f_by <- function(where, what, vals) {
+  if (length(what) > length(where)) {
+    rep(0L, length(vals))
+  }
+  res <- rep(0L, length(vals))
+  for (k in length(what):(length(where) - 1)) {
+    found <- TRUE
+    for (j in 1:length(what)) {
+      if (where[k - length(what) + j] != what[j]) {
+        found <- FALSE
+        break
+      }
+    }
+    if (found) {
+      idx <- where[k + 1]
+      res[idx] <- res[idx] + 1L
+    }
+  }
+  res
+}
+
+find_occurrences <- function(where, what) {
+  res <- as.integer(NULL)
+  if (length(what) > length(where)) {
+    res
+  }
+  for (k in length(what):length(where)) {
+    found <- TRUE
+    for (j in 1:length(what)) {
+      if (where[k - length(what) + j] != what[j]) {
+        found <- FALSE
+        break
+      }
+    }
+    if (found) {
+      res <- c(res, k - length(what) + 1L)
+    }
+  }
+  res
+}
