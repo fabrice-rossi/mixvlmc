14fe24e1f7bf723ead9d614d696dd644ceccc493
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2023-10-08 14:28:17 +0200

    Remove duplicate implementation.

--- tests/testthat/helper-suffix_tree.R
+++ tests/testthat/helper-suffix_tree.R
@@ -18,22 +18,3 @@ count_occurrences <- function(where, what) {
   res
 }
 
-find_occurrences <- function(where, what) {
-  if (length(what) > length(where)) {
-    NULL
-  }
-  res <- NULL
-  for (k in length(what):length(where)) {
-    found <- TRUE
-    for (j in 1:length(what)) {
-      if (where[k - length(what) + j] != what[j]) {
-        found <- FALSE
-        break
-      }
-    }
-    if (found) {
-      res <- c(res, k - length(what) + 1)
-    }
-  }
-  res
-}
