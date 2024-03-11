facb0eba85dee6215909b779e5deee54101259c0
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2024-03-11 19:11:13 +0100

    Move to a faster and less memory hungry likelihood calculation.

---
+++ tests/testthat/helper-vlmc_multi.R
@@ -0,0 +1,27 @@
+# compute the log likelihood using multiple calls to
+# loglikelihood
+loglikelihood_multi <- function(model, xs,
+                                initial = c("truncated", "specific", "extended"),
+                                ignore,
+                                weights = NULL) {
+  initial <- match.arg(initial)
+  ll <- 0
+  nb_obs <- 0L
+  if (is.null(weights)) {
+    weights <- rep(1, length(xs))
+  }
+  for (k in seq_along(xs)) {
+    if (rlang::is_missing(ignore)) {
+      the_ll <- loglikelihood(model, xs[[k]], initial)
+    } else {
+      the_ll <- loglikelihood(model, xs[[k]], initial, ignore)
+    }
+    ll <- ll + weights[k] * as.numeric(the_ll)
+    nb_obs <- nb_obs + weights[k] * attr(the_ll, "nobs")
+  }
+  df <- attr(the_ll, "df")
+  attr(ll, "df") <- df
+  attr(ll, "nobs") <- nb_obs
+  attr(ll, "initial") <- initial
+  structure(ll, class = c("logLikMixVLMC", "logLik"))
+}
