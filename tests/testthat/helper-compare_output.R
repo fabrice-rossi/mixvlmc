e39fe1c6ab8a30125e4888ce348bc718289e21cf
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2024-03-23 10:03:24 +0100

    Add support for keeping the output files to ease debugging.

--- tests/testthat/helper-compare_output.R
+++ tests/testthat/helper-compare_output.R
@@ -1,12 +1,20 @@
 ## compare the console outputs of two functions that should produce
 ## the same things
-compare_output <- function(f, g) {
-  f_tmpfile <- withr::local_tempfile(fileext = ".txt")
+compare_output <- function(f, g, keep_files = FALSE) {
+  if (keep_files) {
+    f_tmpfile <- tempfile(fileext = ".txt")
+  } else {
+    f_tmpfile <- withr::local_tempfile(fileext = ".txt")
+  }
   withr::with_output_sink(
     f_tmpfile,
     eval(substitute(f), envir = parent.frame())
   )
-  g_tmpfile <- withr::local_tempfile(fileext = ".txt")
+  if (keep_files) {
+    g_tmpfile <- tempfile(fileext = ".txt")
+  } else {
+    g_tmpfile <- withr::local_tempfile(fileext = ".txt")
+  }
   withr::with_output_sink(
     g_tmpfile,
     eval(substitute(g), envir = parent.frame())
